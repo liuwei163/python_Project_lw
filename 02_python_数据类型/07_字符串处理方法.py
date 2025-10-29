94e785e50c5737f43620c32cb82ccc44a3238c81
lw <2031260394@qq.com>
2025-10-29 23:38:49 +0800

    1

---
+++ 02_python_数据类型/07_字符串处理方法.py
@@ -0,0 +1,44 @@
+# 方法                               描述
+# str.lower()                        以小写的方式全部返回str的副本
+# str.upper()                        以大写的方式全部返回str的副本
+# str.split(sep=None)                返回一个列表，以sep作为分隔点，sep默认为空格或换行
+# str.count(sub)                     返回sub子串出现的次数
+# str.replace(old,new)               返回字符串str的副本，所有old子串被替换为new
+# str.center(width,fillchar)         字符串居中函数，fillchar参数可选
+# str.strip(chars)                   从字符串str中去掉在其左侧和右侧chars中列出的字符
+# strjoin(iter)                      将iter变量的每一个元素后面（元素和元素中间）增加一个str字符串
+
+# str.lower()
+s = "aBcDeFg"
+print(s.lower())
+
+# str.upper()
+s = "aBcDeFg"
+print(s.upper())
+
+# str.split(sep=None)
+# 以, 号作为分隔符
+s = "北京,上海,天津 重庆"
+print(s.split('.'))
+print(s.split())   # 默认使用空格为分隔符
+
+# str.count(sub)
+s = "abcabcabcababc"
+print(s.count('ac'))
+
+# str,replace(old,new)
+# 字符串中有几个匹配old的内容，都会被替换成new的内容
+s = "北京,上海,天津 重庆 天津天津"
+print(s.replace("天津", "山西"))
+
+# str.center(width,fillchar)
+s = "666小黑666"
+print(s.center(20, "*"))
+
+# str.strip()
+s = "67666小黑66676"
+print(s.strip("6"))
+
+# str.join(iter)
+s = "小黑小白绿蓝红"
+print("+".join(s))
