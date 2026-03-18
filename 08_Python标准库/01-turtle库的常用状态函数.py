84897813212e87be9b80bc0605427bbb5c949173
刘伟 <16555699+only-lw@user.noreply.gitee.com>
2026-03-18 17:44:57 +0800

    Changes

---
+++ 08_Python标准库/01-turtle库的常用状态函数.py
@@ -0,0 +1,82 @@
+"""
+函数                          描述
+pendown()                    放下画笔
+penup()                      拿起画笔
+pensize()                    设置画笔线条的粗细
+pencolor()                   设置画笔颜色
+color()                      设置画笔和填充颜色
+begin_fi()                   填充图形前，调用该方法
+end fill()                   填充图形结束
+filing()                     返回填充状态，True为填充
+clear()                      清空当前窗口
+"""
+
+# pendown()和 penup()
+import turtle
+# 拿起画笔
+turtle.up()
+turtle.fd(200)
+turtle.down()
+turtle.fd(200)
+
+turtle.done()
+
+# pensize()
+import turtle
+
+turtle.pensize(5)
+turtle.fd(200)
+
+turtle.done()
+
+# pencolor()
+import turtle
+
+turtle.pencolor("red")
+turtle.fd(200)
+
+turtle.done()
+
+# color(); begin_fi(); end fill(); turtle.filling(); clear()
+import turtle
+
+# color("")：1.设置画笔颜色，2.设置填充颜色
+turtle.color("blue", "red")
+# 填充开始
+turtle.begin_fill()
+
+# 绘制一个三角形
+for i in range(3):
+    turtle.fd(100)
+    turtle.left(120)
+
+# 返回填充是否正常，成功填充返回True，失败则返回False
+turtle.filling()
+
+# 填充结束
+turtle.end_fill()
+
+# 清空窗口
+turtle.clear()
+
+turtle.done()
+
+"""
+reset()                         清空当前窗口，并重置位置
+write(str,font=None)            输出font字体的字符串
+screensize()                    设置画布的长和宽
+hideturtle()                    隐藏画笔的turtle形状
+showturtle                      显示画笔形状
+Isvisible()                     如果画笔可见则显示true
+"""
+
+# reset()
+import turtle
+
+# color("")：1.设置画笔颜色，2.设置填充颜色
+turtle.color("blue")
+turtle.fd(300)
+# 清空窗口,并重置画笔位置
+turtle.reset()
+
+turtle.done()
--- 08_Python标准库/test_库.py
+++ 08_Python标准库/test_库.py
@@ -1,8 +1,9 @@
 import turtle
-# turtle.setup(200, 100, 100, 100)
-turtle.setup(0.4, 0.5)
 
-# turtle.done() 和 turtle.mainloop() 作用相同，都会启动 turtle 的事件循环，使窗口保持响应。
-# 如果你在交互式环境（如 IDLE、Jupyter Notebook）中逐行执行，窗口可能不会立即关闭，因为 Python 进程仍在运行。
-# 但在脚本模式下（直接运行 .py 文件）必须调用上述函数。
+# color("")：1.设置画笔颜色，2.设置填充颜色
+turtle.color("blue")
+turtle.fd(300)
+# 清空窗口,并重置画笔位置
+turtle.write("liuwei")
+
 turtle.done()
